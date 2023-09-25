a46a199ae3d1069663dcd5ca9aaccb069d69de74
Анастасия Базанова <nastya.is.fine@mail.ru>
2023-09-25 22:46:13 +0300

    junit

---
+++ src/test/java/CashbackHackServiceTest.java
@@ -0,0 +1,35 @@
+package ru.netology.service;
+
+import static org.junit.Assert.assertEquals;
+
+public class CashbackHackServiceTest {
+
+    @org.junit.Test
+    public void testRemain() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 1000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual,expected);
+    }
+
+    @org.junit.Test
+    public void testNotEnough() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 900;
+        int actual = service.remain(amount);
+        int expected = 100;
+        assertEquals(actual,expected);
+
+
+    }
+    @org.junit.Test
+    public void testMoreThan1000() {
+        CashbackHackService service = new CashbackHackService();
+        int amount = 2000;
+        int actual = service.remain(amount);
+        int expected = 0;
+        assertEquals(actual, expected);
+    }
+
+}
